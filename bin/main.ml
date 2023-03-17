b921c4c3bbfb4845d681e98686d79a263caf2250
Willem <willem@matimba.com>
2023-03-17 12:54:42 +0000

    sql

--- bin/main.ml
+++ bin/main.ml
@@ -1,14 +1,15 @@
 open Sqlite3
 
-let create_table_sql = "
-                        CREATE TABLE IF NOT EXISTS contacts (
-	                contact_id INTEGER PRIMARY KEY,
-	                first_name TEXT NOT NULL,
-	                last_name TEXT NOT NULL,
-	                email TEXT NOT NULL UNIQUE,
-	                phone TEXT NOT NULL UNIQUE
-                        );
-"
+let create_table_sql =
+  {|
+   CREATE TABLE IF NOT EXISTS contacts (
+   contact_id INTEGER PRIMARY KEY,
+   first_name TEXT NOT NULL,
+   last_name TEXT NOT NULL,
+   email TEXT NOT NULL UNIQUE,
+   phone TEXT NOT NULL UNIQUE
+   );  |}
+
 let db = db_open "test.db"
 
 let () =
